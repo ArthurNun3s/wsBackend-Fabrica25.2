0c501196d2624c73e1335e611bdc702998f5644a
Arthur Nunes <arthur.nunes.rodrigues@gmail.com>
2025-09-14 14:33:26 -0300

    templates
    criação e ajustes para o mesmo

--- torneio/urls.py
+++ torneio/urls.py
@@ -1,13 +1,12 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import PlayerViewSet, TournamentViewSet, MatchViewSet
-
-
-router = DefaultRouter()
-router.register(r'players', PlayerViewSet, basename='player')
-router.register(r'tournaments', TournamentViewSet, basename='tournament')
-router.register(r'matches', MatchViewSet, basename='match')
+from django.urls import path
+from .views import PlayerListView, PlayerCreateView, PlayerUpdateView, HomePageView 
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', HomePageView.as_view(), name='home'),
+    
+    path('players/', PlayerListView.as_view(), name='lista-jogadores'),
+
+    path('players/new/', PlayerCreateView.as_view(), name='cria-jogador'),
+
+    path('players/<int:pk>/edit/', PlayerUpdateView.as_view(), name='edita-jogador'),
 ]
--- torneio/views.py
+++ torneio/views.py
@@ -1,17 +1,22 @@
+from django.views.generic import ListView, CreateView, UpdateView, TemplateView 
+from django.urls import reverse_lazy
+from .models import Player
 
-from rest_framework import viewsets
-from .models import Player, Tournament, Match  
-from .serializers import PlayerSerializer, TournamentSerializer, MatchSerializer 
+class HomePageView(TemplateView):
+    template_name = 'home.html'
 
-class PlayerViewSet(viewsets.ModelViewSet):
-    queryset = Player.objects.all()
-    serializer_class = PlayerSerializer
+class PlayerListView(ListView):
+    model = Player
+    template_name = 'lista_jogadores.html'
 
-class TournamentViewSet(viewsets.ModelViewSet):
-    queryset = Tournament.objects.all()
-    serializer_class = TournamentSerializer
+class PlayerCreateView(CreateView):
+    model = Player
+    template_name = 'cria_jogador.html'
+    fields = ['name', 'role', 'character']
+    success_url = reverse_lazy('lista-jogadores')
 
-class MatchViewSet(viewsets.ModelViewSet):
-    queryset = Match.objects.all()
-    serializer_class = MatchSerializer
-
+class PlayerUpdateView(UpdateView):
+    model = Player
+    template_name = 'edita_jogador.html' 
+    fields = ['name', 'role', 'character']
+    success_url = reverse_lazy('lista-jogadores')
